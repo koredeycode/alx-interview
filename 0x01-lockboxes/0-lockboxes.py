f8c96f25211dc58c370fcd6a2bbac4aae265142f
koredeycode <koredey4u@gmail.com>
2023-04-27 15:10:39 +0000

    done?

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""canUnlockAll function present"""
+from typing import List, Set
+
+
+def canUnlockAll(boxes: List[List[int]]) -> bool:
+    """The canUnlockAllFunction"""
+    unlocked_boxes: List[bool] = [True] + [False] * (len(boxes) - 1)
+
+    seen_boxes: Set[int] = set([0])
+
+    boxes_to_visit: List[int] = list(boxes[0])
+
+    while boxes_to_visit:
+        box_index: int = boxes_to_visit.pop(0)
+        if not unlocked_boxes[box_index]:
+            unlocked_boxes[box_index] = True
+            seen_boxes.add(box_index)
+            boxes_to_visit.extend(boxes[box_index])
+
+    return len(seen_boxes) == len(boxes)
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,3 @@
+# 0x01-lockboxes
+In this project, I learned about lockboxes as part of my projects for the ALX Software Engineering Programme
+* Koredeycode
