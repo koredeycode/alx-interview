a7dfca51e52e790fb7059b35f3d8eb580700d597
koredeycode <koredey4u@gmail.com>
2023-05-11 22:44:44 +0000

    this

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,30 +6,39 @@ module documentation
 import sys
 import re
 
-pattern = r'^([\d.]+) - \[(.*?)\] "GET \/projects\/260 HTTP\/1\.1" (\d+) (\d+)$'
-count = 0
-size = 0
-stats = {}
-try:
-    for line in sys.stdin:
-        line = line.strip()
 
-        if not re.match(pattern, line):
-            continue
+def main():
+    """
+    main function
+    """
+    p = r'^([\d.]+) - \[(.*?)\] "GET \/projects\/260 HTTP\/1\.1" (\d+) (\d+)$'
+    pattern = p
+    count = 0
+    size = 0
+    stats = {}
+    try:
+        for line in sys.stdin:
+            line = line.strip()
 
-        match = re.match(pattern, line)
-        file_size = int(match.group(4))
-        status_code = int(match.group(3))
+            if not re.match(pattern, line):
+                continue
 
-        size += file_size
-        stats[status_code] = stats.get(status_code, 0) + 1
-        count += 1
-        if count % 10 == 0:
-            print("File size: {}".format(size))
-            for code in sorted(stats.keys()):
-                print("{}: {}".format(code, stats[code]))
-except KeyboardInterrupt:
-    print("File size: {}".format(size))
-    for code in sorted(stats.keys()):
-        print("{}: {}".format(code, stats[code]))
+            match = re.match(pattern, line)
+            file_size = int(match.group(4))
+            status_code = int(match.group(3))
 
+            size += file_size
+            stats[status_code] = stats.get(status_code, 0) + 1
+            count += 1
+            if count % 10 == 0:
+                print("File size: {}".format(size))
+                for code in sorted(stats.keys()):
+                    print("{}: {}".format(code, stats[code]))
+    except KeyboardInterrupt:
+        print("File size: {}".format(size))
+        for code in sorted(stats.keys()):
+            print("{}: {}".format(code, stats[code]))
+
+
+if __name__ == "__main__":
+    main()
