cda7e8debc80480766bcd2a60edbd7d1e2556ff4
koredeycode <koredey4u@gmail.com>
2023-05-11 22:34:05 +0000

    this

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -10,20 +10,26 @@ pattern = r'^([\d.]+) - \[(.*?)\] "GET \/projects\/260 HTTP\/1\.1" (\d+) (\d+)$'
 count = 0
 size = 0
 stats = {}
-for line in sys.stdin:
-    line = line.strip()
+try:
+    for line in sys.stdin:
+        line = line.strip()
 
-    if not re.match(pattern, line):
-        continue
+        if not re.match(pattern, line):
+            continue
 
-    match = re.match(pattern, line)
-    file_size = int(match.group(4))
-    status_code = int(match.group(3))
+        match = re.match(pattern, line)
+        file_size = int(match.group(4))
+        status_code = int(match.group(3))
 
-    size += file_size
-    stats[status_code] = stats.get(status_code, 0) + 1
-    count += 1
-    if KeyboardInterrupt or count % 10 == 0:
-        print("File size: {}".format(size))
-        for code in sorted(stats.keys()):
-            print("{}: {}".format(code, stats[code]))
+        size += file_size
+        stats[status_code] = stats.get(status_code, 0) + 1
+        count += 1
+        if count % 10 == 0:
+            print("File size: {}".format(size))
+            for code in sorted(stats.keys()):
+                print("{}: {}".format(code, stats[code]))
+except KeyboardInterrupt:
+    print("File size: {}".format(size))
+    for code in sorted(stats.keys()):
+        print("{}: {}".format(code, stats[code]))
+
