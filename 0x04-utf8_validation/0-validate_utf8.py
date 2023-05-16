ad15750d56b6e988305d8c9f71ddaad821aadec2
koredeycode <koredey4u@gmail.com>
2023-05-16 21:51:55 +0000

    this

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 """
 Module documentation for the utf-8 validation task
 """
@@ -9,15 +8,12 @@ def validUTF8(data: List[int]) -> bool:
     """
     determines if a given data set represents a valid utf-8 encoding
     """
-    # Number of bytes in the current UTF-8 character
     num_bytes = 0
 
     for byte in data:
-        # Check the 8 least significant bits of the byte
         byte = byte & 0xFF
 
         if num_bytes == 0:
-            # Determine the number of bytes for the next character
             if (byte >> 7) == 0b0:
                 num_bytes = 1
             elif (byte >> 5) == 0b110:
@@ -29,11 +25,9 @@ def validUTF8(data: List[int]) -> bool:
             else:
                 return False
         else:
-            # Check if the byte starts with the continuation pattern "10"
             if (byte >> 6) != 0b10:
                 return False
 
         num_bytes -= 1
 
-    # If there are remaining bytes, it means the data is incomplete
     return num_bytes == 0
