c4c977b22d871a0763ec3683250facbe9b79aacd
koredeycode <koredey4u@gmail.com>
2023-08-07 07:14:33 +0000

    refactored

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,23 +4,33 @@ const request = require('request');
 
 const film = process.argv[2];
 
-const main = () => {
-  request.get(`https://swapi-api.alx-tools.com/api/films/${film}`, (err, _, body) => {
-    if (err) {
-      console.log(err);
-    }
-    const characters = JSON.parse(body).characters;
-    const charactersName = characters.map(
-      (url) => new Promise((resolve, reject) => {
-        request(url, (perr, _, pbody) => {
-          if (perr) {
-            reject(perr);
-          }
-          resolve(JSON.parse(pbody).name);
-        });
-      })
-    );
-    Promise.all(charactersName).then(names => console.log(names.join('\n'))).catch(allErr => console.log(allErr));
+const fetchCharacterName = (url) => {
+  return new Promise((resolve, reject) => {
+    request.get(url, (err, _, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.parse(body).name);
+      }
+    });
   });
 };
+
+const main = () => {
+  request.get(`https://swapi-api.alx-tools.com/api/films/${film}`, async (err, _, body) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    try {
+      const characters = JSON.parse(body).characters;
+      const characterPromises = characters.map(fetchCharacterName);
+      const characterNames = await Promise.all(characterPromises);
+      console.log(characterNames.join('\n'));
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
+
 main();
