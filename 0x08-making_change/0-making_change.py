5d00b0f8ed2d6918c05d569690794887d039e55b
koredeycode <koredey4u@gmail.com>
2023-08-22 08:43:33 +0000

    this

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,7 +1,9 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
 """
 the makeChange module
 """
+
+
 def makeChange(coins: list, total: int) -> int:
     """
     determine the fewest number of coins needed to make total
