4e00c330502f2ce7dbdc2327a1a4afb0b6d23946
koredeycode <koredey4u@gmail.com>
2023-08-22 08:40:51 +0000

    initialized-directory

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,9 @@
+#\!/usr/bin/python3
+"""
+the makeChange module
+"""
+def makeChange(coins: list, total: int) -> int:
+    """
+    determine the fewest number of coins needed to make total
+    """
+    pass
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,3 @@
+# 0x08-making_change
+In this project, I learned about making_change as part of my projects for the ALX Software Engineering Programme
+* Koredeycode
