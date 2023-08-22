11f881f374e2c162b2dfdf512a54f1e5e6d012cc
koredeycode <koredey4u@gmail.com>
2023-08-22 13:21:33 +0000

    check

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,4 +8,18 @@ def makeChange(coins: list, total: int) -> int:
     """
     determine the fewest number of coins needed to make total
     """
-    pass
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for amount in range(1, total + 1):
+        for coin in coins:
+            if amount >= coin:
+                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    if dp[total] == float('inf'):
+        return -1
+
+    return dp[total]
